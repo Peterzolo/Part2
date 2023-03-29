f094a3051f71479aa024695e3e87f9c27c75a908
Peter <petersolomon704@gmail.com>
2023-03-29 11:20:38 +0100

    Update Form.jsx
    Modified code

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -1,15 +1,20 @@
 import React, { useState } from "react";
 
 const Form = () => {
-  const [newNote, setNewNote] = useState("hghghghghgg");
+  const [newNote, setNewNote] = useState("");
   console.log("NEW NOTE", newNote);
   const addNote = (event) => {
     event.preventDefault();
   };
+
+  const handleNoteChange = (event) => {
+    console.log(event.target.value);
+    setNewNote(event.target.value);
+  };
   return (
     <div>
       <form onSubmit={addNote}>
-        <input value={newNote} />
+        <input value={newNote} onChange={handleNoteChange} />
         <button type="submit">save</button>
       </form>
     </div>
