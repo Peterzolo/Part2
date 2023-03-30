91cfce306425e033242116927990ed44ef48e3a4
Peter <petersolomon704@gmail.com>
2023-03-30 14:58:33 +0100

    Modified
    Modified codes

--- src/components/DB/DB.jsx
+++ src/components/DB/DB.jsx
@@ -1,12 +1,5 @@
 import React from "react";
 
-import axios from "axios";
-
-axios.get("http://localhost:3001/notes").then((response) => {
-  const notes = response.data;
-  console.log(notes);
-});
-
 const DB = () => {
   return <div>DBkhlklkjl</div>;
 };
